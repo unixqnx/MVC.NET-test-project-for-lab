0a9fbadf24994255eaf425486948a01ee12c805e
unixqnx <unixqnx@mail.ru>
2017-04-08 01:16:45 +0600

    First lecture - simple CRUD application;

---
+++ test/test/Controllers/RoleController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace test.Controllers
+{
+    public class RoleController : Controller
+    {
+        // GET: Role
+        public ActionResult Index()
+        {
+            IEnumerable<Role> roles;
+
+            using (testDBEntities context = new testDBEntities())
+            {
+                roles =  context.Roles.ToList();
+            }
+
+            return View(roles);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            Role role;
+            using (testDBEntities context = new testDBEntities())
+            {
+                role = context.Roles.Find(id);
+            }
+
+            return View(role);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Role role)
+        {
+            using (testDBEntities context = new testDBEntities())
+            {
+                context.Entry(role).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            Role role;
+            using (testDBEntities context = new testDBEntities())
+            {
+                role = context.Roles.Find(id);
+            }
+
+            return View(role);
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(Role role)
+        {
+            using (testDBEntities context = new testDBEntities())
+            {
+                context.Entry(role).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ test/test/Controllers/StaffController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            IEnumerable<Staff> staffs;
+
+            using (testDBEntities context = new testDBEntities())
+            {
+                staffs = context.Staffs.Include("Role").ToList();
+            }
+
+            return View(staffs);
+        }
+
+        public ActionResult Add()
+        {
+            using (testDBEntities context = new testDBEntities())
+            {
+                ViewBag.Roles = context.Roles.ToList();
+            }
+            return View(new Staff());
+        }
+
+        [HttpPost]
+        public ActionResult Add(Staff staff)
+        {
+            using (testDBEntities context = new testDBEntities())
+            {
+                context.Staffs.Add(staff);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
