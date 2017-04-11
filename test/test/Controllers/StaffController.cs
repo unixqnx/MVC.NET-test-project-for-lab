afa7ac878e3a6e1f14fed31dfca61839b142706c
Dmitriy_Sankevich <Dmitriy_Sankevich@epam.com>
2017-04-11 17:06:30 +0600

    Add json Ajax;

--- test/test/Controllers/StaffController.cs
+++ test/test/Controllers/StaffController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Formatting = System.Xml.Formatting;
 
 namespace test.Controllers
 {
@@ -21,7 +23,64 @@ namespace test.Controllers
         }
 
 
-        //[ChildActionOnly]
+		#region
+
+	    public ActionResult Index2(int? id)
+        {
+            using (testDBEntities context = new testDBEntities())
+            {
+                ViewBag.Roles = context.Roles.ToList();
+            }
+
+            return View(id);
+        }
+
+	    public ActionResult Index3(int? id)
+        {
+            using (testDBEntities context = new testDBEntities())
+            {
+                ViewBag.Roles = context.Roles.ToList();
+            }
+
+            return View(id);
+        }
+
+		public JsonResult GetStaffsDataJson(int? id)
+        {
+			IEnumerable<Staff> staffs;
+			using (testDBEntities context = new testDBEntities())
+			{
+				context.Configuration.LazyLoadingEnabled = false;
+				if (id == null)
+				{
+					staffs = context.Staffs.Include("Role").ToList();
+				}
+				else
+				{
+					staffs = context.Staffs.Include("Role").Where(_ => _.Role.id == id).ToList();
+				}
+			}
+
+			var result =
+				staffs.Select(
+					_ =>
+						new Staff
+						{
+							id = _.id,
+							Name = _.Name,
+							RoleId = _.RoleId,
+							Role = new Role { id = _.Role.id, roleName = _.Role.roleName}
+						}).ToList();
+			return Json(result, JsonRequestBehavior.AllowGet);
+
+			//var list = JsonConvert.SerializeObject(staffs, Newtonsoft.Json.Formatting.None, new JsonSerializerSettings() { ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore });
+			//return Content(list, "application/json");
+        }
+
+		#endregion
+
+
+		//[ChildActionOnly]
         public ActionResult GetStaffs(int? id)
         {
             IEnumerable<Staff> staffs;
