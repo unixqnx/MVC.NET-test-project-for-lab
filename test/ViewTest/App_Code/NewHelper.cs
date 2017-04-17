25b6af94a09c4c3b2e9a2df5527ad9b3bbde1a65
Dmitriy_Sankevich <Dmitriy_Sankevich@epam.com>
2017-04-17 11:04:46 +0600

    Add View examples;

---
+++ test/ViewTest/App_Code/NewHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewTest.App_Code
+{
+    public static class NewHelper
+    {
+        public static MvcHtmlString MyHelper(string name)
+        {
+            return new MvcHtmlString($"<input type='submit' value='{name}'/>");
+        }
+    }
+}
---
+++ test/ViewTest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Now(DateTime date)
+        {
+            return PartialView("Now", date);
+        }
+    }
+}
